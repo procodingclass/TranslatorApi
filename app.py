bf8de56e8f3f51fbbb59dcad1a4897b1a7826879
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-02-09 12:53:28 +0530

    Adding CORS permissions
    Adding CORS origin permissions to the API

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from googletrans import Translator as gTrans
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def welcome():
@@ -20,7 +22,12 @@ def welcome():
 def hello_world():
     return 'Hello, World!'
 
-
+# CORS Headers 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
